dbd69f6f65c66ef4e5fe414140a3ce227b25b47e
aaronr8684 <57102156+aaronr8684@users.noreply.github.com>
2022-02-15 11:48:44 -0500

    Update remote_install.sh
    Changing for test...will revert later

--- install/remote_install.sh
+++ install/remote_install.sh
@@ -10,7 +10,7 @@ set -eu
 
 #PP_GITREPO="https://github.com/txoof/PaperPi.git"
 PP_GITREPO="git@github.com:txoof/PaperPi.git"
-PP_GITBRANCH="manage_modules"
+PP_GITBRANCH="aaronr8684-patch-auto_enable_spi"
 
 INSTALLER="/install/install.sh"
 
