0565b362117f70478c10b8109240ab50d299f48e
txoof <aaron.ciuffo@gmail.com>
2022-11-24 08:22:34 +0100

    ignore in ini creation

--- paperpi/plugins/default/constants.py
+++ paperpi/plugins/default/constants.py
@@ -7,3 +7,15 @@ data = {
   'digit_time': '00:00',
   'msg': 'str',
 }
+
+# do not include a sample INI configuration
+include_ini = False
+
+sample_config='''
+[Plugin: default fallback plugin]
+layout = layout
+plugin = default
+refresh_rate = 30
+min_display_time = 60
+max_priority = 2**15
+'''
