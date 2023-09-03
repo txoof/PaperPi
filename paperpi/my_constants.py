6496afcf6c49eefb72858a3c459f5f20e2195565
txoof <aaron.ciuffo@gmail.com>
2023-09-03 15:46:36 +0200

    main config follows plugin format
    rewrite to match standard plugin format
    
    fix typo
    
    fix typo

--- paperpi/my_constants.py
+++ paperpi/my_constants.py
@@ -36,13 +36,30 @@ FONTS = Path(f'{BASE_DIRECTORY}/fonts').resolve()
 
 # required keys that every plugin configuration needs to have
 REQ_PLUGIN_KEYS = {
-    'layout': 'layout',
-    'plugin': None,
-    'refresh_rate': 60,
-    'min_display_time': 60,
-    'max_priority': 2
+  "layout": {
+    "description": "Layout to use in displaying plugin output",
+    "value": "layout"
+  },
+  "plugin": {
+    "description": "Formal plugin name",
+    "value": "None"
+  },
+  "refresh_rate": {
+    "description": "Time in seconds between requests for new data from plugin. This is **not** the display time.",
+    "value": 60,
+    "type": "int"
+  },
+  "min_display_time": {
+    "description": "Minimum time in seconds plugin should display before a new plugin is displayed",
+    "value": 60,
+    "type": "int"
+  },
+  "max_priority": {
+    "description": "Maximum priority this plugin should reach. This is best left at the plugin default (typically 2) unless you **really** know what you're doing.",
+    "value": 2,
+    "type": "int"
+  }
 }
-
 
 LOGGING_CONFIG = Path(CONFIG_PATH)/'logging.cfg'
 
